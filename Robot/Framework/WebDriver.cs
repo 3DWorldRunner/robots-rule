39d7b9dd81421bb96ef78252564e5be2c14d0fac
Jeremiah Hall <Jeremiah.Hall@SkyKick.com>
2018-08-27 12:41:20 -0700

    Renamed webdriver class

---
+++ Robot/Framework/WebDriver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Robot.Framework
+{
+    public class WebDriver
+    {
+        public IWebDriver Driver
+        {
+            get
+            {
+                if (_driver == null)
+                {
+                    _driver = new ChromeDriver();
+                    _driver.Manage().Window.Maximize();
+                }
+
+                return _driver;
+            }
+            set => _driver = value;
+        }
+        private static IWebDriver _driver;
+    }
+}
